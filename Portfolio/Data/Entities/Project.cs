4419b10f27ba0c5229a7aea2ff776fbcae6c3d43
erhangndz <erhangndz@live.com>
2026-07-14 22:20:29 +0300

    updated

--- Portfolio/Data/Entities/Project.cs
+++ Portfolio/Data/Entities/Project.cs
@@ -1,13 +1,25 @@
-﻿namespace Portfolio.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Data.Entities
 {
     public class Project
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Görsel Url boş bırakılamaz.")]
         public string ImageUrl { get; set; }
+
+        [Required(ErrorMessage = "Proje Adı boş bırakılamaz.")]
+        [MinLength(3,ErrorMessage ="Proje Adı en az 3 karakter olmalıdır.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Proje Açıklaması boş bırakılamaz.")]
+        [MaxLength(100,ErrorMessage ="Proje Açıklaması en fazla 100 karakter olmalıdır.")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Github Url boş bırakılamaz.")]
         public string GithubUrl { get; set; }
-        public List<ProjectTechStack> ProjectTechStacks { get; set; }
+        public List<ProjectTechStack>? ProjectTechStacks { get; set; }
 
     }
 }
